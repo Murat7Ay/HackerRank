4de5d82ef33166cb86e0ecfb3f39ade2ec2ce219
Murat Ay <murataybtp@gmail.com>
2018-07-19 18:57:35 +0300

    OutMemoryException error must fix

---
+++ HowManySubstrings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class HowManySubstrings
+    {
+
+        //TODO:OutOfMemoryException hatasına çözüm lazım.
+        public int[] CountSubstrings(string text, int[][] queries)
+        {
+            List<int> sum = new List<int>();
+
+
+            foreach (var query in queries)
+            {
+                if (query.Length != 2)
+                    throw new ArgumentOutOfRangeException();
+                var sample = Slice(text,query.First(), query.Last());
+                sum.Add(CalculateSubStrings(sample, sample.Length));
+            }
+            return sum.ToArray();
+        }
+
+        private int CalculateSubStrings(string s, int n)
+        {
+            List<string> allPossible = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                for (int len = 1; len <= n - i; len++)
+                {
+                    allPossible.Add(s.Substring( i, len));
+                }
+            }
+            return allPossible.Distinct().Count();
+        }
+
+        public static string SubStringCustom(string source,int start, int end)
+        {
+            return string.Join("", source.Select(s => s.ToString()).Skip(start).Take(end - start));
+        }
+
+        public static string Slice(string source, int start, int end)
+        {
+            return  string.Join( "",source.Select(s=>s.ToString()).Where((x,i)=>i>=start&&i<=end).Select(s=>s));
+        }
+
+        public static int GetValues(string source, int start, int end)
+        {
+            Dictionary<string,object> dictionary = new Dictionary<string, object>();
+            var slicedText = string.Join("", source.Select(s => s.ToString()).Where((x, i) => i >= start && i <= end).Select(s => s));
+
+
+
+
+        }
+    }
+}
